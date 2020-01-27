0f8272674482e30ecc30e89be2dee6c00a1f0aa7
Mohamed Abdallah <mr.mohamedabdallah98@gmail.com>
2020-01-27 10:42:46 +0200

    Add Authentication guards

--- angular/src/app/app-routing.module.ts
+++ angular/src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { RegisterComponent } from './comps/auth/register/register.component';
 import { NotFoundComponent } from './comps/not-found/not-found.component';
 import { SearchComponent } from './comps/search/search.component';
 import { UserComponent } from './comps/user/user.component';
+import { IsAuthGuard } from './guards/is-auth.guard';
+import { IsNotAuthGuard } from './guards/is-not-auth.guard';
 
 
 const routes: Routes = [
   {path: "", component: HomeComponent},
-  {path: "login", component: LoginComponent},
-  {path: "register", component: RegisterComponent},
+  {path: "login", component: LoginComponent, canActivate: [IsNotAuthGuard]},
+  {path: "register", component: RegisterComponent, canActivate: [IsNotAuthGuard]},
   {path: "search/:query", component: SearchComponent},
   {path: "user/:id", component: UserComponent},
   {path: "404", component: NotFoundComponent},
---
+++ angular/src/app/guards/is-not-auth.guard.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class IsNotAuthGuard implements CanActivate {
+
+  constructor(private _router: Router, private _auth: AuthService){}
+
+  
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+      if(!this._auth.isLogged){
+        return true;
+      } else {
+        this._router.navigate(['/']);
+      }
+      return false;
+  }
+  
+}
