0993b7bc2cd914e91686e21334ce0dfa80fc89f1
Mohamed Abdallah <mr.mohamedabdallah98@gmail.com>
2020-02-03 02:26:11 +0200

    Storing the file data on the image form input
    Get the file from file input or drag-drop action and storing its
    data on the image form control waiting to be uploaded

--- angular/src/app/comps/upload/upload.component.ts
+++ angular/src/app/comps/upload/upload.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 import { FormGroup, FormControl, FormArray } from '@angular/forms';
 
 @Component({
@@ -6,9 +6,11 @@ import { FormGroup, FormControl, FormArray } from '@angular/forms';
   templateUrl: './upload.component.html',
   styleUrls: ['./upload.component.scss']
 })
-export class UploadComponent implements OnInit {
+export class UploadComponent implements OnInit, AfterViewInit {
 
   @ViewChild("fileInput", {static: false}) fileInput: ElementRef;
+  @ViewChild("img", {static: false}) image: ElementRef;
+  @ViewChild("dropBox", {static: false}) dropBox: ElementRef;
 
   uploadForm: FormGroup;
   
@@ -27,13 +29,59 @@ export class UploadComponent implements OnInit {
       ]),
       image: new FormControl(null)
     });
+    this.uploadForm.valueChanges.subscribe(console.log);
   }
+  
+  ngAfterViewInit(){
+    // File input change event
+    this.fileInput.nativeElement.addEventListener("change", (e: any)=>{
+      this.storeFileValue(this.fileInput.nativeElement.files[0]);
+    });
+
+    // DropBox events
+    let box = this.dropBox.nativeElement;
+    
+    // Drop event
+    box.addEventListener("drop", (e: any) =>{
+      e.stopPropagation();
+      e.preventDefault();
+      
+      this.storeFileValue(e.dataTransfer.files[0]);
+    })
+    // Drag over the element event
+    box.addEventListener("dragover", (e: any) =>{
+      e.stopPropagation();
+      e.preventDefault();
+      
+      e.dataTransfer.dropEffect = "copy";
+    })
+
+  }
+
+
 
   /**
    * Click to the upload box event
    */
   boxClicked(){
     this.fileInput.nativeElement.click();
+  }
+
+
+
+  /**
+   * Show the image
+   * Store file object to the image input
+   * @param file 
+   */
+  storeFileValue(file: File){
+    
+    // Create url of the file
+    let url = URL.createObjectURL(file);
+    this.image.nativeElement.src = url;
+  
+    // Store the file on "image" input
+    this.uploadForm.get("image").setValue(file);
   }
 
 }
