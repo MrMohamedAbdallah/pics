9a4cef85565cd1055165edb8261f143583f8299e
Mohamed Abdallah <mr.mohamedabdallah98@gmail.com>
2020-02-04 06:08:20 +0200

    Make user's email visible when getting user's data

--- laravel/app/User.php
+++ laravel/app/User.php
@@ -27,7 +27,7 @@ class User extends Authenticatable implements JWTSubject
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token', 'created_at', 'updated_at', 'email', 'email_verified_at'
+        'password', 'remember_token', 'created_at', 'updated_at', 'email_verified_at'
     ];
 
     /**
