31ec69d297df4d2233307126c46a44191bf89562
Mohamed Abdallah <mr.mohamedabdallah98@gmail.com>
2020-03-05 22:57:38 +0200

    README.md and design files

---
+++ README.md
@@ -0,0 +1,68 @@
+# **Pics 📸**
+-------------------
+**Unsplash clone made with Angular and Laravel with Algolia API ❤**
+
+### **Setup**
+---------------
+1. Clone or download the project.
+
+2. Create the database and configure `.env` file.
+
+3. Run this command for storage configuration in larvel.
+    ```bash
+    php artisan storage:linke
+    ``` 
+
+4. Add [algolia](https://www.algolia.com/) app key and secret to the `.env` file.
+
+5. Import `images` table data into Alogila app folowing this [guide](https://www.algolia.com/doc/framework-integration/laravel/indexing/import-into-algolia/?language=php).
+
+
+**What I learned from it 😊**
+* Using Angular with Laravel
+* Using tymon/jwt-auth for authentication
+* Using ngx-toastr for flash messages
+
+### **Tools, Framworks And Libraries**
+--------------------------------------
+1. Design
+    * Adobe XD
+    * Illustrator (for the logo)
+2. Backend
+    * Laravel
+    * Laravel Scout with Algolia
+3. Frontend
+    * Angular
+    * ngx-toastr
+    * Colcade
+    * Gulp
+    * Pug
+    * Sass
+    
+## **Design** 
+------------------------
+_all text are lorem ipsum_
+
+#### **Logo**
+![Underground logo](design/logo.jpg)
+
+#### **Home**
+![Home Page](design/Home.jpg)
+
+#### **Search**
+![search](design/search.jpg)
+
+#### **Display Picture**
+![picture](design/picture.jpg)
+
+#### **Login**
+![Login](design/login.jpg)
+
+See the design on 
+[Behance](https://www.behance.net/gallery/93323065/Pics)
+
+Video preview for the website [Vimeo](https://vimeo.com/395806882)
+
+My
+[Twitter](https://twitter.com/MrMohamed98) | 
+[Linkedin](https://www.linkedin.com/in/mohamed-abdallah-b731b61a2/)
--- angular/src/app/comps/images/modal/modal.component.ts
+++ angular/src/app/comps/images/modal/modal.component.ts
@@ -1,21 +1,26 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
-import { ImageService } from 'src/app/services/image.service';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { AuthService } from 'src/app/services/auth.service';
+import { Component, OnInit, Input, EventEmitter, Output } from "@angular/core";
+import { ImageService } from "src/app/services/image.service";
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { AuthService } from "src/app/services/auth.service";
+import { MessagesService } from 'src/app/services/messages.service';
 
 @Component({
-  selector: 'app-modal',
-  templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  selector: "app-modal",
+  templateUrl: "./modal.component.html",
+  styleUrls: ["./modal.component.scss"]
 })
 export class ModalComponent implements OnInit {
-
   image: any = null;
   index: number = null;
   showModal: boolean = false;
   user: any = null;
   @Output("delete") deleteEmitter: EventEmitter<any> = new EventEmitter<any>();
-  constructor(private _imageService: ImageService, private _http: HttpClient, private _auth: AuthService) { }
+  constructor(
+    private _imageService: ImageService,
+    private _http: HttpClient,
+    private _auth: AuthService,
+    private _msgService: MessagesService
+  ) {}
 
   ngOnInit() {
     this._imageService.showModal.subscribe(data => {
@@ -25,60 +30,66 @@ export class ModalComponent implements OnInit {
     });
 
     this.user = this._auth.getUser();
-    this._auth.userObserver.subscribe((user)=>{
+    this._auth.userObserver.subscribe(user => {
       this.user = user;
     });
   }
-  
-  hideModal(){
+
+  hideModal() {
     this.showModal = false;
   }
 
-  download(e){
+  download(e) {
     e.preventDefault();
 
-    
-      let a = document.createElement("a");
-      let url = "http://pics.test/api/download/" + this.image.id
-      a.href = url;
-      a.download = url;
-      // start download
-      a.click();
-    
+    let a = document.createElement("a");
+    let url = "http://pics.test/api/download/" + this.image.id;
+    a.href = url;
+    a.download = url;
+    // start download
+    a.click();
   }
 
   /**
    * Delete an image
    */
-  delete(){
-    if(confirm("Are you sure you want to delete this image?")){
+  delete() {
+    if (confirm("Are you sure you want to delete this image?")) {
       /**
        * Get the access token
        * Send a delete request to the server
        * Delete the image from the view
        */
-      
-       let access_token = this._auth.access_token;
-      
-       let headers = new HttpHeaders().set("Accept", "application/json").set("Authorization", "Bearer " + access_token);
 
-       this._http.delete("http://pics.test/api/images/" + this.image.id, {
-         headers: headers
-       }).subscribe(
-         (data)=>{
-           // Delete the image from the view
+      let access_token = this._auth.access_token;
+
+      let headers = new HttpHeaders()
+        .set("Accept", "application/json")
+        .set("Authorization", "Bearer " + access_token);
+
+      this._http
+        .delete("http://pics.test/api/images/" + this.image.id, {
+          headers: headers
+        })
+        .subscribe(
+          data => {
+            // Delete the image from the view
             this.deleteEmitter.emit(this.index);
+
+            // Show success message
+            this._msgService.success(
+              "Congratulation",
+              "The image deleted successfully"
+            );
+
             // Hide the modal
             this.hideModal();
-         },
-         (err)=>{
-           console.error("Error while deleting the image");
-         },
-         ()=>{},
-       );
-       
-
+          },
+          err => {
+            console.error("Error while deleting the image");
+          },
+          () => {}
+        );
     }
   }
-
 }
